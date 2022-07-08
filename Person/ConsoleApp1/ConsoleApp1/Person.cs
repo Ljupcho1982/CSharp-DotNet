bf7eaf9e5e5e0a4158cb0b17e9d6295a9e1fd9e3
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-07-08 21:32:37 +0200

    Person

---
+++ Person/ConsoleApp1/ConsoleApp1/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Person
+    {
+
+        public string name { get; set; }
+
+        public int height { get; set; }
+
+        public string eyeColor { get; set; }
+
+        public int weight { get; set; }
+
+        public string rase { get; set; }
+
+        public void Person1()
+        {
+            Console.WriteLine("$The height is:  {1}cm the eyes are: {2} the weight is: {3}kg the rase is: {4}",name,height,eyeColor,weight,rase);
+
+        }
+    }
+}
---
+++ Person/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp1;
+
+Console.WriteLine("Hello, World!");
+
+Person Ljupcho = new Person();
+Ljupcho.name = "Ljupcho";
+Ljupcho.height = 186;
+Ljupcho.eyeColor = "green";
+Ljupcho.weight = 63;
+Ljupcho.rase = "caucasion";
+
+Ljupcho.Person1();
