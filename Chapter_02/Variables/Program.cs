a9e30ea643b51322567768120a9bc29e62406cfd
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-07-13 01:05:06 +0200

    13.07.2022 C#

---
+++ Chapter_02/Variables/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using System.Globalization;
+using System.Xml;
+// See https://aka.ms/new-console-template for more information
+
+object height = 1.88;
+object name = "Amir";
+Console.WriteLine($"{name} is {height} meters tall");
+
+//int length1 = name.Length;
+
+int length2=((string)name).Length;
+Console.WriteLine($"{name} has {length2} characters.");
+
+dynamic something = "Ahmet";
+something = new[] {88,99,8};
+
+
+// int doesnt have a lenght property
+//and array of any type has a length property
+//Console.WriteLine($"Length is: {something.Length}");
+
+// good use of var because it avoids the repeated type
+// as shown in the more verbose second statement
+var xml1 = new XmlDocument();
+XmlDocument xml2 = new XmlDocument();
+// bad use of var because we cannot tell the type, so we
+// should use a specific type declaration as shown in
+// the second statement
+var file1 = File.CreateText("something1.txt");
+StreamWriter file2 = File.CreateText("something2.txt");
+
+Console.WriteLine($"default(int)={default(int)}");
+Console.WriteLine($"default(bool)={default(bool)}");
+Console.WriteLine($"default(DateTime)={default(DateTime)}");
+Console.WriteLine($"default(string)={default(string)}");
+string[] array;
+array = new string[4];
+array[0] = "Ljupcho";
+array[1] = "KKK";
+array[2] = "KJJ";
+array[3] = "KJJ";
+
+for (int i=0; i<array.Length; i++)
+{
+Console.WriteLine(array[i]);
+    
+
+}
+
+
+
+
+
---
+++ Projects/Control_Fllow_Statements_and_Loops/Control_Fllow_Statements_and_Loops/Program.cs
@@ -0,0 +1,171 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//If Statement
+if (1 < 2)
+{
+
+
+    if (false)
+
+    {
+
+        Console.WriteLine("You hit the second if");
+    }
+    else
+    {
+        Console.WriteLine("Else was hit");
+    }
+}
+
+//Switch statement
+
+int value = 2;
+
+switch (value)
+{
+
+    case 1:
+        Console.WriteLine("1");
+        break;
+    case 2:
+        Console.WriteLine("2");
+        break;
+    case 3:
+        Console.WriteLine("3");
+        break;
+    case 4:
+        Console.WriteLine("4");
+        break;
+    case 5:
+        Console.WriteLine("5");
+        break;
+    case 6:
+        Console.WriteLine("6");
+        break;
+    case 7:
+        Console.WriteLine("7");
+        break;
+    case 8:
+        Console.WriteLine("8");
+        break;
+    case 9:
+        Console.WriteLine("9");
+        break;
+    case 10:
+        Console.WriteLine("10");
+        break;
+    default:
+    Console.WriteLine("0");
+        break;
+}
+//switch statement days of the week
+string message = "";
+int day = 3;
+switch (day)
+{
+    case 1:
+        message="Sunday";
+        break;
+
+    case 2:
+        message="Monday";
+        break;
+    case 3:
+        message = "Thusday";
+        break;
+
+    case 4:
+        message = "Wednesday";
+        break;
+
+    case 5:
+        message = "Thursday";
+        break;
+    case 6:
+        message = "Friday";
+        break;
+    case 7:
+        message = "Saturday";
+        break;
+    default:
+        message = "No day";
+        break;
+}
+Console.WriteLine(message);
+
+//for loops
+
+for(int i = 0; i < 5; i++)
+{
+    await Task.Delay(100);
+    Console.WriteLine("Hello World");
+}
+string example = "abc";
+
+for(int i = 0; i < example.Length; i++)
+{
+    await Task.Delay(100);
+
+    Console.WriteLine(example[i]);
+};
+
+// for loop revers
+
+for(int i = example.Length - 1; i >= 0; i--)
+{
+    await Task.Delay(100);
+    Console.WriteLine(example[i]);
+}
+
+//outside foor loop
+//int i = 0;
+
+/* for (; ; ){
+
+    if (i < 4)
+    {
+
+        Console.WriteLine(i.ToString());
+        i++;
+    }
+};*/
+
+// nested loop
+
+for (int i = 0; i < 5; i++)
+{
+    for(int j = 0; j < 5; j++)
+    {
+
+        Console.WriteLine("i= "+ i.ToString() + " " + "j= " + j.ToString());
+    }
+
+}
+
+//foreach loop
+
+List<string> myFriends = new List<string> {
+    "Emma", "Rupert", "Daniel", "Maggie", "Alan"
+};
+
+Console.WriteLine("Enter a valid name!");
+var name = Console.ReadLine();
+int index = myFriends.IndexOf(name);
+Console.WriteLine(index);
+/*
+int index = 0;
+foreach (var friend in myFriends)
+{
+   Console.WriteLine($"Friend {index}: {friend}");
+    index++;
+}
+*/
+
+//select
+
+
+// index of in for loop
+
+
+
+   
