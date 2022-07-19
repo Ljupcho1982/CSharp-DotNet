0270ce30efd90148efd874ea1ba8f489aca4c806
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-07-19 22:19:05 +0200

    Chaning Colors

---
+++ Projects/User_Input_Files/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+Console.WriteLine("g=green,r=red, b=blue,w=white");
+int y = Console.Read();
+char userInput = Convert.ToChar(y);
+while (userInput != 'z')
+{
+
+    switch (userInput)
+    {
+
+        case 'g':
+        Console.BackgroundColor = ConsoleColor.Green;
+            break;
+        case 'r':
+            Console.BackgroundColor = ConsoleColor.Red;
+            break;
+        case 'w':
+            Console.BackgroundColor = ConsoleColor.White;
+            break;
+        default:
+            break;
+       
+            
+    }
+    Console.Clear();
+
+   
+}
