331c5717ecc618f21db219f48fe90ccd7259a2b9
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-07-24 23:49:10 +0200

    24.07.2022 11:49

---
+++ Projects/Asynchronous Functions/MakeDinner.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Asynchronous_Functions
+{
+    internal class MakeDinner1
+    {
+        static async Task CookMeat()
+        {
+
+            Console.WriteLine("Cook Meat");
+        }
+        static async Task CookVegetables()
+        {
+
+            Console.WriteLine("Cook Vegetables");
+        }
+
+        static async Task MakeBread()
+        {
+
+            Console.WriteLine("Make Bread");
+        }
+
+        static async Task MakeSoup()
+        {
+
+            Console.WriteLine("Make Soup");
+        }
+
+        static async Task MakeSalad()
+        {
+
+            Console.WriteLine("Make Salad");
+        }
+
+        static async Task MakeDrinks()
+        {
+
+            Console.WriteLine("Make Drinks");
+        }
+
+         static async void MakeDinner()
+        {
+
+            await CookMeat();
+            await CookVegetables();
+            await MakeBread();
+            await MakeSoup();
+            await MakeDrinks();
+           
+
+        }
+    }
+}
---
+++ Projects/Asynchronous Functions/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Asynchronous_Functions;
+
+//MakeDinner1 dinner=new MakeDinner1();
+
+//dinner.MakeDinner();
+
+static async Task CookMeat()
+{
+
+    Console.WriteLine("Cook Meat");
+}
+static async Task CookVegetables()
+{
+
+    Console.WriteLine("Cook Vegetables");
+}
+
+static async Task MakeBread()
+{
+
+    Console.WriteLine("Make Bread");
+}
+
+static async Task MakeSoup()
+{
+
+    Console.WriteLine("Make Soup");
+}
+
+static async Task MakeSalad()
+{
+
+    Console.WriteLine("Make Salad");
+}
+
+static async Task MakeDrinks()
+{
+
+    Console.WriteLine("Make Drinks");
+}
+
+static async void MakeDinner()
+{
+
+    await CookMeat();
+    await CookVegetables();
+    await MakeBread();
+    await MakeSoup();
+    await MakeDrinks();
+
+
+}
+MakeDinner();
+
+
