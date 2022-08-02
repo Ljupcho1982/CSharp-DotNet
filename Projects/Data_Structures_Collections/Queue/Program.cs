2ab96d302531e53539a9cf19413dff87bf3d2af1
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-08-02 20:39:53 +0200

    queue

---
+++ Projects/Data_Structures_Collections/Queue/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+using System.Diagnostics;
+
+Queue<string> queue = new Queue<string>();
+queue.Enqueue("a");
+queue.Enqueue("b");
+queue.Enqueue("c");
+
+foreach(var item in queue)
+{
+
+    Console.WriteLine(item);
+}
+System.Diagnostics.Debug.WriteLine(queue.Dequeue());
+System.Diagnostics.Debug.WriteLine(queue.Peek());
---
+++ Projects/Data_Structures_Collections/Stack/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+Stack<string> s = new Stack<string>();
+
+s.Push("a");
+s.Push("b");
+
+Console.WriteLine("The stack contains " + s.Count + " elements");
+
+
+private static bool isBalanced(string inputString)
+{
+
+    Stack<char> stackOfOpeningBraces = new Stack<char>();
+    Stack<char> stackOfClosingBraces = new Stack<char>();
+
+    foreach(char c in inputString)
+    {
+
+        if(c=='}' || c==']'|| c==')')
+        {
+
+            stackOfClosingBraces.Push(c);
+        }
+    }
+
+    for(int i=inputString.Length-1; i>=0; i--)
+    {
+
+        if(inputString[i]=='{' || inputString[i]=='[' || inputString[i] == '(')
+        {
+
+            stackOfOpeningBraces.Push(inputString[i]);
+        }
+        if((stackOfOpeningBraces.Count+stackOfClosingBraces)%2 != 0)
+        {
+
+            return false;
+        }
+
+        while (stackOfClosingBraces.Count !=0)
+        {
+            char currentClosingBraces = stackOfClosingBraces.Pop();
+            char currentOpeningBraces = stackOfOpeningBraces.Pop();
+        if((currentClosingBraces=='}' && currentOpeningBraces=='{') || (currentClosingBraces==']' && currentClosingBraces==']')|| (currentClosingBraces==')' && currentOpeningBraces=='[')||(currentClosingBraces==')' && currentClosingBraces == '('))
+            {
+
+                continue;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+}
