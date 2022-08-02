a4b2191a8a093dc74efddeb56c4cb424cc591650
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-08-02 21:52:26 +0200

    dictionaries

---
+++ Projects/Data_Structures_Collections/Dictionaries/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+
+Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+dictionary.Add("key1", "value1");
+
+dictionary.Add("key2", "value1");
+dictionary.Add("key3", "value3");
+foreach (string key in dictionary.Keys)
+{
+
+    Console.WriteLine(key);
+}
+
+foreach(string value in dictionary.Values)
+{
+
+    Console.WriteLine(value);
+}
+
+if (!dictionary.ContainsKey("key1"))
+{
+
+    dictionary.Add("key1","value1")
+}
+else
+{
+    Console.WriteLine("key1 exists");
+}
+
+if (dictionary.ContainsKey("key3"))
+{
+
+    Console.WriteLine(dictionary["key3"]);
+};
+
+string value = "";
+
+dictionary.TryGetValue("key1", out value);
