7c1273f986e25f88ed145bb174439485c8db1fe5
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-08-02 22:11:36 +0200

    HashSets

---
+++ Projects/Data_Structures_Collections/HashSets/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+HashSet<string> letters1 = new HashSet<string>() { "a", "b", "c" };
+HashSet<string> letters2 = new HashSet<string>() { "d", "e", "e","a","b" };
+//letters1.UnionWith(letters2);
+//foreach(string s in letters1)
+//{
+//    Console.WriteLine(s);
+//}
+
+Console.WriteLine("-------------------------------------------");
+//letters1.IntersectWith(letters2);
+
+//foreach(string s in letters1)
+//{
+//    Console.WriteLine(s);
+//}
+
+//Console.WriteLine("-------------------------------------------");
+
+//letters1.ExceptWith(letters2);
+
+//foreach (string s in letters1)
+//{
+//    Console.WriteLine(s);
+//}
+
+
+letters1.SymmetricExceptWith(letters2);
+
+foreach (string s in letters1)
+{
+    Console.WriteLine(s);
+}
