6465f7ead8c210887af9e6c45684528891d8de60
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-08-02 21:38:56 +0200

    enum

---
+++ Projects/Data_Structures_Collections/Enums/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using static System.Enum;
+
+
+
+
+
+
+try
+{
+
+    foreach (string s in GetNames(typeof(Weekdays)))
+    {
+
+        Console.WriteLine(s);
+    }
+
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message, ex.StackTrace);
+}
+
+enum Weekdays { Monday = 5, Tuesday, Wednesday, Friday, kkkkk, kkkkkk, jjj };
+
