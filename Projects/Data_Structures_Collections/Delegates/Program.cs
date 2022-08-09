75006105066b4373afed499db9d7224862617620
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-08-09 22:10:08 +0200

    Delegates

---
+++ Projects/Data_Structures_Collections/Delegates/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Print print = null;
+print += HelloWorld;
+print += GoodByeWorld;
+
+
+print();
+
+
+ void GoodByeWorld()
+{
+
+    Console.WriteLine("GoodBye World");
+}
+
+
+void HelloWorld()
+{
+
+    Console.WriteLine("Hello World");
+}
+
+delegate void Print();
+
+
