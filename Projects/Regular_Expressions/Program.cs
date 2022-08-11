9ffdf8753388fb9a9c12b87dbddf7803318f8416
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-08-11 22:28:21 +0200

    RegularExpressions

---
+++ Projects/Regular_Expressions/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+string pattern = @"\d";
+Regex regex = new Regex(pattern);
+
+Console.WriteLine("Does 2 match pattern: "+regex.IsMatch("2"));
+Console.WriteLine("Does a match pattern: "+regex.IsMatch("a"));
+
+string pattern1 = "(the)";
+Regex regex1 = new Regex(pattern1);
+string text = "the quick brown fox jumped over the lazy dog";
+Match match1 = regex.Match(text);
+MatchCollection matches = regex.Matches(text);
