a088fc7874be633938d7e4cea881f7ea38444780
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-08-11 21:42:59 +0200

    Towers of Hanoi

---
+++ Projects/Data_Structures_Collections/Towers of Hanoi/Program.cs
@@ -0,0 +1,16 @@
+﻿void MoveDisk(int n, Stack<int> tower1, Stack<int> tower2, Stack<int> tower3)
+{
+    if (n == 1)
+    {
+
+        tower3.Push(tower1.Pop());
+        return;
+
+    }
+
+    MoveDisk(n - 1, tower1, tower2, tower1);
+    tower3.Push(tower1.Pop());
+    MoveDisk(n - 1, tower2, tower3, tower1);
+
+}
+
