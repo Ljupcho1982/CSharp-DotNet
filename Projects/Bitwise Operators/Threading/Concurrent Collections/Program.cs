1ea254dcc00b0a78832a7ee166e530ebbb2391da
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-09-06 10:54:33 +0200

    Concurrent Collections

---
+++ Projects/Bitwise Operators/Threading/Concurrent Collections/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+ConcurrentBag<int> bag=new ConcurrentBag<int> ();
+List<Task> tasks=new List<Task> ();
+for (int i = 0; i < 100; i++)
+{
+
+    var numberAdd = i;
+    tasks.Add(Task.Run(()=>bag.Add(numberAdd)));
+}
+
+Task.WaitAll(tasks.ToArray());
+
+List<Task> runningTasks = new List<Task> ();
+int numberOfItems = 0;
+
+while (!bag.IsEmpty)
+{
+    runningTasks.Add(Task.Run(() =>
+    {
+        int item;
+
+        if (bag.TryTake(out item))
+        {
+
+            Console.WriteLine(item);
+            numberOfItems++;
+        }
+
+    }));
+
+};
+
+Task.WaitAll (runningTasks.ToArray());
+
+Console.WriteLine($"There were {numberOfItems} items in the bag");
---
+++ Projects/Bitwise Operators/Threading/PararlelPrgraming/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+
+Parallel.Invoke(
+
+    ()=>new WebClient().DownloadFile("http://www.google.com","google.html"),
+    ()=>new WebClient().DownloadFile("http://www.yahoo.com","yahoo.html")
+
+
+    );
+Parallel.For(0, 200, i => Console.WriteLine("Hello World"));
