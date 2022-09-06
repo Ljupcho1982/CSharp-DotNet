5753ab07134e11d3fdf98b165fa55d4d9bac1f8c
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-09-06 11:16:57 +0200

    Finding the oldestPerson

---
+++ Projects/LINQ/LINQ/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using LINQ;
+
+
+List<Person> people = new List<Person>()
+{
+
+    new Person() {Name="John", Age=30},
+    new Person() {Name="Sarah", Age=25},
+    new Person() {Name="James", Age=32},
+    new Person() {Name="Joseph", Age=24},
+
+
+};
+
+int oldestPerson = people.Select(x => x.Age).Max();
+Console.WriteLine(oldestPerson);
+
+
+
