ac78382e37d94a8d2b4397c05daee3472fac95e6
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-09-06 22:51:42 +0200

    Intersection,Exception and Union

---
+++ Projects/LINQ/Distinct, Except, Intersect and Union/Program.cs
@@ -0,0 +1,34 @@
+﻿//Union
+List<int> collection1=new List<int>() { 1,2,3,4,5};
+List<int> collection2=new List<int>(){ 5,6,7,8,9};
+
+IEnumerable<int> collection1and2=collection1.Union(collection2);
+
+foreach(int item in collection1and2) {
+
+    Console.WriteLine(item);
+
+}
+
+//intersection
+
+Console.WriteLine("_____________________________________________________");
+IEnumerable<int> collectionIntersection=collection1.Intersect(collection2);
+
+foreach(int i in collectionIntersection)
+{
+    
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("_____________________________________________________");
+
+//Exception
+
+IEnumerable<int> exception  = collection1.Except(collection2);
+
+foreach(int i in exception)
+{
+
+    Console.WriteLine(i);
+}
