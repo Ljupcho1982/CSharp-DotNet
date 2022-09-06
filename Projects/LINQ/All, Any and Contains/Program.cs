47aac78fc6087c6a3aa7b9cfde4637a7f5690fd1
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-09-06 23:12:12 +0200

    All, Any and Contains

---
+++ Projects/LINQ/All, Any and Contains/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+Console.WriteLine($"Are all elements greater than 0? {numbers.All(x=>x>0)}");
+
+Console.WriteLine($"Are any element greater than 6? {numbers.Any(x=>x>6)}");
+
+Console.WriteLine($"Does the list contain 3? {numbers.Contains(3)} ");
