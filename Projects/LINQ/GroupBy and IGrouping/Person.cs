4760c49485a1696798e915ad1cdd53d2101abeff
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-09-06 22:24:24 +0200

    GroupBy_and_IGrouping

---
+++ Projects/LINQ/GroupBy and IGrouping/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupBy_and_IGrouping
+{
+    public class Person
+    {
+
+        public string FirstName;
+        public string LastName;
+
+    }
+}
---
+++ Projects/LINQ/GroupBy and IGrouping/Program.cs
@@ -0,0 +1,36 @@
+﻿using GroupBy_and_IGrouping;
+
+using System.Collections.Generic;
+using System.Linq;
+
+
+//GroupBy
+
+
+List<Person> people = new List<Person>()
+{
+
+    new Person(){FirstName="John", LastName="Smith"},
+
+    new Person(){FirstName="Sally", LastName="Smith"},
+
+    new Person(){FirstName="Thomas",LastName="Gioia"},
+
+    new Person (){FirstName="Robert", LastName="Gioia"}
+
+
+};
+
+IEnumerable<IGrouping<string, Person>> groupedPeople = people.GroupBy(person => person.LastName);
+
+foreach (IGrouping<string, Person> groupedPerson in groupedPeople)
+{
+
+    Console.WriteLine($"Key : {groupedPerson.Key}");
+
+    foreach(var person in groupedPerson)
+    {
+        Console.WriteLine($"\t{person.LastName}, {person.FirstName}");
+
+    }
+}
