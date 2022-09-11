5eb1a7dffd2d5697f8490fa3995bc7eea909d3c4
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-09-11 11:40:27 +0200

    Code without and with Refactoring

---
+++ Projects/Refactoring/Program.cs
@@ -0,0 +1,34 @@
+﻿//Without Refactoring
+List<string> ExampleData=new List<string>() { "sample","solution", "programming","solve","computer"};
+
+ string [] words;
+
+words = new string[5];
+
+for (int i = 0; i < ExampleData.Count; i++)
+{
+
+    if (ExampleData[i][0].Equals('s'))
+    {
+
+        words[i] = ExampleData[i];
+    }
+}
+
+for(int i =0; i < words.Length; i++)
+{
+
+    Console.WriteLine(words[i]);
+}
+
+//With Refactoring
+
+for(int i=0; i < ExampleData.Count; i++)
+{
+
+    if (ExampleData[i][0].Equals('s'))
+    {
+
+        Console.WriteLine(ExampleData[i]);
+    }
+}
