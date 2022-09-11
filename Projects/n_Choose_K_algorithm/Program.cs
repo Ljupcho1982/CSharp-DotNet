20c4aa0ac83465687132c268eaba6a9621258373
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-09-11 12:30:53 +0200

    N choose K Algorithm

---
+++ Projects/n_Choose_K_algorithm/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int numberOfPossibleCombinations(int n, int k)
+{
+
+    if (k == 0 || k==n)
+    {
+        return 1;
+
+    }
+    else
+    {
+
+        return numberOfPossibleCombinations(n - 1, k) + numberOfPossibleCombinations(n - 1, k - 1);
+
+    }
+}
+
+Console.WriteLine(numberOfPossibleCombinations(3,2));
