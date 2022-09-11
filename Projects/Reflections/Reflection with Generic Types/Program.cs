718eafd8db250d779c76a73485e075503ec27e9f
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-09-11 10:43:48 +0200

    Refelctions

---
+++ Projects/Reflections/Reflection with Generic Types/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+
+
+Console.WriteLine("Is string generic type?" + typeof(string).IsGenericType);
+
+Console.WriteLine("Is list generic type?" + typeof(List<>).IsGenericType);
+
+Type t = typeof(Dictionary<,>);
+
+if (t.IsGenericType)
+{
+
+    Type[] types = t.GetGenericArguments();
+
+    Console.WriteLine("Number of type parameters ={0}", types.Length);
+
+    foreach(Type param in types)
+    {
+
+        if (param.IsGenericParameter)
+        {
+
+            Console.WriteLine("Generic Type = {0}", param);
+        }
+        else
+        {
+
+            Console.WriteLine("Type={0}",param);
+        }
+    }
+
+
+}
---
+++ Projects/Reflections/Reflections/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+
+Type t = typeof(int);
+
+MemberInfo[] typeMembers= t.GetMembers();
+
+foreach(MemberInfo memberInfo in typeMembers)
+{
+
+    Console.WriteLine("Member = " + memberInfo);
+}
+
+FieldInfo[] typeFields=t.GetFields();
+
+foreach(FieldInfo field in typeFields)
+{
+
+    Console.WriteLine("Field= " + field);
+}
+
+MethodInfo[] typeMethods = t.GetMethods();
+
+foreach(MethodInfo method in typeMethods)
+{
+
+    Console.WriteLine("Methods = " + method);
+}
+
